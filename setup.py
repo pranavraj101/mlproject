e4509250075245dc96188fd3162b033443724264
Pranav <sinhapranavraj10142@gmail.com>
2023-04-14 18:05:20 +0530

    logging and exception and file structure build

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path)->List[str]:
+    #this function will return a list of requirements
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
+setup(
+name = "mlproject",
+version = "0.0.1",
+author = "pranav",
+author_email = "sinhapranavraj10142@gmail.com",
+packages=find_packages(),
+install_requires = get_requirements('requirements.txt'),
+
+)
